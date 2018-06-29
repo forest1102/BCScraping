63c5108a92d1c0637645141a4dccc3460a64fa7b
issei morita <isseimorita1@gmail.com>
2018-06-28 19:28:21 -0700

    title variable value to one file

--- src/scraping.ts
+++ src/scraping.ts
@@ -2,6 +2,7 @@ import * as moment from 'moment'
 import { BCDetailURL, BCItemListURL, BCStockURL, AmazonURL } from './serialize'
 import * as Rx from 'rx'
 import { withDelay } from './customObs'
+import { shopLength, dataKeys, defaultStock, dataTitle, amazonKey, shopLists } from './titles'
 
 type detailObject = {
 	'商品名': string
@@ -26,85 +27,6 @@ enum StockType {
 	'◎ 在庫あり',
 	'在庫無し'
 }
-export const dataKeys = [
-	'商品コード',
-	'JANコード',
-	'商品名',
-	'型番'
-]
-
-const dataTitle = [
-	'商品コード',
-	'JANコード',
-	'商品名',
-	'型番',
-	'価格（税込）',
-	'ポイント'
-]
-
-const amazonKey = [
-	'price',
-	'rank'
-]
-
-
-const shopLists = [
-	"ビックカメラ 池袋本店",
-	"ビックカメラ 池袋本店パソコン館",
-	"ビックカメラ 池袋東口カメラ館",
-	"ビックカメラ 池袋西口店",
-	"ビックカメラ アウトレット池袋東口店",
-	"ビックカメラ 有楽町店",
-	"ビックカメラ 赤坂見附駅店",
-	"ビックカメラ AKIBA",
-	"ソフマップAKIBA1号店 サブカル・モバイル館",
-	"ソフマップAKIBA2号店 パソコン総合館",
-	"ビックカメラ 新宿西口店",
-	"ビックロ　ビックカメラ新宿東口店",
-	"ビックカメラ 新宿東口駅前店",
-	"ビックドラッグ シダックス新宿セントラルロード店",
-	"ビックカメラセレクト原宿店",
-	"ビックカメラ 渋谷東口店",
-	"ビックカメラ 渋谷ハチ公口店",
-	"ビックカメラ 立川店",
-	"ビックカメラ JR八王子駅店",
-	"ビックカメラ 京王調布店",
-	"ビックカメラ 聖蹟桜ヶ丘駅店",
-	"ビックカメラ セレクト町田店",
-	"ビックカメラ ラゾーナ川崎店",
-	"ビックカメラ 横浜西口店",
-	"ビックカメラ 新横浜店",
-	"ビックカメラ 藤沢店",
-	"ビックカメラ 相模大野駅店",
-	"ビックカメラ 柏店",
-	"ビックカメラ 船橋駅FACE店",
-	"ビックカメラ 船橋東武店",
-	"ビックカメラ 大宮西口そごう店",
-	"ソフマップ川越店",
-	"ビックカメラ 高崎東口店",
-	"ビックカメラ 水戸駅店",
-	"ビックカメラ 札幌店",
-	"ビックカメラ 新潟店",
-	"ビックカメラ 浜松店",
-	"ビックカメラ 名古屋駅西店",
-	"ビックカメラ 名古屋JRゲートタワー店",
-	"ビックトイズ プライムツリー赤池店",
-	"ビックカメラ JR京都駅店",
-	"ビックカメラ なんば店",
-	"ビックカメラ あべのキューズモール店",
-	"ソフマップ神戸ハーバーランド店",
-	"ビックカメラ 岡山駅前店",
-	"ビックカメラ 広島駅前店",
-	"ビックカメラ 天神１号館",
-	"ビックカメラ 天神２号館",
-	"ビックカメラ 鹿児島中央駅店",
-	"Air BIC CAMERA羽田空港国際線店※",
-	"Air BIC CAMERA アクアシティお台場店※",
-	"Air BIC CAMERA 成田空港第２ターミナル店※",
-	"Air BIC CAMERA 中部国際空港セントレア店(国際線出発ゲート内)※"
-]
-const shopLength = shopLists.length
-const defaultStock = new Array(shopLength).fill('0')
 
 export const scrapingItemListObservable = (queries: SearchObject) =>
 	Rx.Observable.if(
